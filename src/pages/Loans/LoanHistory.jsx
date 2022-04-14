f1964d897fde19e136f44f38d1ac4471d742fad4
Charles Kasasira <charleskasasira01@gmail.com>
2022-04-14 15:26:33 +0300

    loan history

--- src/pages/Loans/LoanHistory.jsx
+++ src/pages/Loans/LoanHistory.jsx
@@ -7,14 +7,14 @@ function LoanHistory() {
       <div className="flex bg-white p-6 min-h-full">
         <div className="w-full relative overflow-x-auto sm:rounded-lg">
           <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
-            <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+            <thead className='text-xs text-gray-700 uppercase dark:bg-gray-700 dark:text-gray-400'>
               <tr>
-                <th className='px-6 py-3'>Date</th><th className='px-6 py-3'>Amount to Pay</th><th className='px-6 py-3'>Amount Paid</th><th className='px-6 py-3'>Principal</th><th className='px-6 py-3'>Interest</th><th className='px-6 py-3'>Status</th>
+                <th className='px-6 py-4'>Date</th><th className='px-6 py-4'>Amount to Pay</th><th className='px-6 py-4'>Amount Paid</th><th className='px-6 py-4'>Principal</th><th className='px-6 py-4'>Interest</th><th className='px-6 py-4'>Status</th>
               </tr>
             </thead>
             <tbody>
               {loanHistory.map((loan, index) => (
-                <tr>
+                <tr className={`${index % 2 === 0 ? "bg-gray-50" : ""} hover:bg-gray-100`} key={index}>
                   <td className='px-6 py-3'>{loan.date}</td><td className='px-6 py-3'>{loan.amountToPay}</td><td className='px-6 py-3'>{loan.amountPaid}</td><td className='px-6 py-3'>{loan.principal}</td><td className='px-6 py-3'>{loan.interest}</td><td className='px-6 py-3'>{loan.status}</td>
                 </tr>
               ))}
