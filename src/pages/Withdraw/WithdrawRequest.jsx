9132e9e5cf4efa840de70ac4f5984f4919d07bc8
Charles Kasasira <charleskasasira01@gmail.com>
2022-04-16 14:38:37 +0300

    make transactions

--- src/pages/Withdraw/WithdrawRequest.jsx
+++ src/pages/Withdraw/WithdrawRequest.jsx
@@ -11,18 +11,24 @@ function WithdrawRequest() {
               <div className='flex flex-wrap gap-5'>
                   <div className='flex flex-col w-56'>
                     <label htmlFor="" className='text-sm'>Please select an account</label>
-                    <input type="text" name="" id="" placeholder='Enter postal address' className='ring-1 ring-black rounded px-2 py-1' />
+                    <select name="" id="" className="ring-1 ring-black rounded px-2 py-2 bg-white">
+                      <option value="">--Select Account--</option>
+                      <option value="savings">Savings</option>
+                      <option value="shares">Shares</option>
+                      <option value="fixed">Fixed</option>
+                      <option value="mwana">Mwana</option>
+                    </select>
                   </div>
                   <div className='flex flex-col w-56 '>
                     <label htmlFor="" className=' text-sm'>Enter amount to withdraw</label>
-                    <input type="text" name="" id="" placeholder='Enter postal address' className='ring-1 ring-black rounded px-2 py-1' />
+                    <input type="text" name="" id="" placeholder='Enter amount' className='ring-1 ring-black rounded px-2 py-1' />
                   </div>
               </div>
               </form>
           </div>
           <div className='mb-3'>
               <h1 className='font-semibold'>Practiculars</h1>
-              <textarea name="" id="" cols="30" rows="10" className='outline outline-1 rounded-md w-full'></textarea>
+              <textarea name="" id="" cols="30" rows="10" className='outline outline-1 p-2 rounded-md w-full'></textarea>
           </div>
         <div className="w-56">
           <Submit value='Request' />
