d514d397576ac1315185389c203360a1b143f50f
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2022-05-10 13:47:16 +0300

    compare dates helper functions

---
+++ api/helpers/compareDates.js
@@ -0,0 +1,11 @@
+/**
+ * @function
+ * @name compareDates
+ * @param { object } date_1 An instance of the date class.
+ * @param { object } date_2 An instance of the date class.
+ * @returns { number } -1: if date1 < date2, 1: if date1 > date2, and, 0: if date1 is equal to date2.
+ */
+
+const compareDates = ( date_1, date_2 ) => isFinite(date_1.valueOf()) && isFinite(date_2.valueOf()) ? (date_1 > date_2) - (date_1 < date_2) : NaN
+
+module.exports = compareDates
