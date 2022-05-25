ad6aeedfaa70b89e29bf4e0f2440300683b520ff
Charles Kasasira <charleskasasira01@gmail.com>
2022-05-25 15:38:29 +0300

    added darkmode to the localStorage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { MdKeyboardArrowDown } from 'react-icons/md'
 import { useAuth } from '../auth/AuthContext'
@@ -7,12 +7,11 @@ import ProfileModal from './Modals/ProfileModal'
 
 function Navbar({ user }) {
   const [ show, setShow ] = useState(false)
+  const { darkMode, toggleDarkMode } = useAuth()
   const navigate = useNavigate()
-  // const navRef = useRef()
-  const { darkMode, setDarkMode } = useAuth()
 
-  if(show === true){
-    window.onclick = function(event) {
+  if(show){
+    window.onclick = event => {
         if (!event.target.matches('.dialog')) {
             setShow(false)
         }
@@ -20,48 +19,46 @@ function Navbar({ user }) {
   }
 
   return (
-    <div className={` dark:bg-dark-bg-700 fixed bg-white  z-5 right-0 top-0 flex navbar ${user?.fullname === undefined || user?.fullname === null ? 'justify-between' : 'justify-end'} items-center p-2`}>
-          {(user?.fullname === undefined || user.fullname === null) && <div className='bg-accent-red p-2 flex justify-center items-center text-white rounded-md cursor-pointer'
+    <div className={` dark:bg-dark-bg-700 fixed bg-white  z-5 right-0 top-0 flex navbar ${!user?.fullname ? 'justify-between' : 'justify-end'} items-center p-2`}>
+          {(!user?.fullname) && <div className='bg-accent-red p-2 flex justify-center items-center text-white rounded-md cursor-pointer'
           onClick={() => {
             navigate('/application')
           }}>
             <p>Complete the application</p>
           </div>}
-          <div className='mx-3'>
-            <DarkModeSwitch
-              style={{ marginBottom: '0' }}
-              checked={darkMode}
-              onChange={() => {
-                const local = localStorage.getItem("darkMode")
-                setDarkMode(darkMode => !darkMode)
-                localStorage.setItem("darkMode", !local)
-              }}
-              size={30}
-            />
-          </div>
-           <div className='flex items-end relative mr-5' 
-            onClick={(event) => {setShow(!show);event.stopPropagation()}}
-          >
-            <div className='flex items-center cursor-pointer dialog'>
-              <div className='text-center'>
-                <p className='mb-0 cursor-pointer dark:text-white'>Hello  
-                {(user?.fullname !== undefined && user.fullname !== null)
-                ? ` ${user?.fullname.split(' ')[0]}`
-                : ' Newbie !!'}
-                </p>
-                  <p className={`text-sm ${user?.member_status === 'active' ? 'text-green-600' : 'text-accent-red'}`}>{user?.member_status ? user.member_status : 'status'}
-                </p>
-              </div>
-              { user?.avatar ? <img src={`${user?.avatar}`} alt="profile"/> :
-                <div className='w-10 h-10 bg-accent dark:bg-dark-bg-600 dark:text-secondary-text rounded-full mx-2 flex justify-center font-bold items-center overflow-hidden'>
-                  {(user?.fullname !== undefined && user.fullname !== null) && ` ${user?.fullname.split('')[0]}`}
-                </div>
-              }
-              <i className='dark:text-white'>
-                <MdKeyboardArrowDown />
-              </i>
+          <div className='flex items-center'>
+            <div className='mx-3'>
+              <DarkModeSwitch
+                style={{ marginBottom: '0' }}
+                checked={darkMode}
+                onChange={() => toggleDarkMode()}
+                size={30}
+              />
             </div>
-            <ProfileModal show={show} setShow={setShow} />
+            <div className='flex items-end relative mr-5'
+              onClick={(event) => {setShow(!show);event.stopPropagation()}}
+            >
+              <div className='flex items-center cursor-pointer dialog'>
+                <div className='text-center'>
+                  <p className='mb-0 cursor-pointer dark:text-white'>Hello
+                  {(user?.fullname !== undefined && user.fullname !== null)
+                  ? ` ${user?.fullname.split(' ')[0]}`
+                  : ' Newbie !!'}
+                  </p>
+                    <p className={`text-sm ${user?.member_status === 'active' ? 'text-green-600' : 'text-accent-red'}`}>{user?.member_status ? user.member_status : 'status'}
+                  </p>
+                </div>
+                { user?.avatar ? <img src={`${user?.avatar}`} alt="profile"/> :
+                  <div className='w-10 h-10 bg-accent dark:bg-dark-bg-600 dark:text-secondary-text rounded-full mx-2 flex justify-center font-bold items-center overflow-hidden'>
+                    {(user?.fullname !== undefined && user.fullname !== null) && ` ${user?.fullname.split('')[0]}`}
+                  </div>
+                }
+                <i className='dark:text-white'>
+                  <MdKeyboardArrowDown />
+                </i>
+              </div>
+              <ProfileModal show={show} setShow={setShow} />
+            </div>
           </div>
       </div>
   )
