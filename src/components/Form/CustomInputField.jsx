934bf80601062880fab374fd92a947535243b141
Charles Kasasira <charleskasasira01@gmail.com>
2022-05-30 16:29:52 +0300

    loan application form one;

--- src/components/Form/CustomInputField.jsx
+++ src/components/Form/CustomInputField.jsx
@@ -2,6 +2,6 @@ export const InputField = ({ errors, touched, reference, id, label, handleChange
     <div className='flex flex-col w-56'>
         <label htmlFor={id} className='text-sm'>{label}</label>  
         <input type="text" className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600' onChange={handleChange(reference)} onBlur={handleBlur(reference)} {...props}/>
-        {touched[reference] && errors[reference] && <div className="error">{errors[reference]}</div>}
+        {touched[reference] && errors[reference] && <div className="error text-xs text-red-500">{errors[reference]}</div>}
     </div>
 )
--- src/pages/Loans/ApplicationPg1.jsx
+++ src/pages/Loans/ApplicationPg1.jsx
@@ -3,6 +3,8 @@ import { supabase } from '../../helpers/supabase'
 import { useAuth } from '../../auth/AuthContext'
 import { toast, ToastContainer } from 'react-toastify'
 import { useOutletContext, useNavigate } from "react-router-dom"
+import { loan1ValidationSchema } from '../../helpers/validator'
+import { InputField } from '../../components/Form/CustomInputField'
 
 export default function ApplicationPg1({ profile, initialValues, setInitialValues, setPageNumber }) {
 
@@ -15,7 +17,9 @@ export default function ApplicationPg1({ profile, initialValues, setInitialValue
         onSubmit={async ( values ) => {
             setInitialValues(values)
             setPageNumber(2)
+            console.log(values)
         }}
+        validationSchema={loan1ValidationSchema}
       >
         {({values, errors, touched, handleChange, handleBlur}) => {
   return (
@@ -43,20 +47,19 @@ export default function ApplicationPg1({ profile, initialValues, setInitialValue
                             }
                         </select>
                     </div>
-                    <div className='flex flex-col w-56 '>
-                    <label htmlFor="" className=' text-sm'>Postal Address</label>
-                    <input type="text" name="postal_address" id="" placeholder='Enter postal address' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary'
-                        onChange={handleChange("postal_address")}
-                    />
-                    </div>
-                    <div className='flex flex-col w-56 '>
-                    <label htmlFor="" className=' text-sm'>Land line number</label>
+                    <InputField errors={errors} touched={touched} handleChange={handleChange}  handleBlur={handleBlur} reference="postal_address"  label="Postal Address" placeholder="Enter Postal Address"/>
+
+                    <InputField errors={errors} touched={touched} handleChange={handleChange}  handleBlur={handleBlur} reference="landline_number"  label="Landline Number" placeholder="Enter number"/>
+
+                    {/* <div className='flex flex-col w-56 '>
+                    <label className=' text-sm'>Land line number</label>
                     <input type="text" name="landline_number" placeholder='(222) 222 - 2222' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary' 
-                        onChange={handleChange("landline_number")}
+                        onChange={handleChange("landline_number")} required
                     />
-                    </div>
+                    </div> */}
+
                     <div className='flex flex-col w-56 '>
-                        <label htmlFor="" className=' text-sm'>Marital Status</label>
+                        <label className=' text-sm'>Marital Status</label>
                         <select name="marital_status" id="" defaultValue={profile?.marital_status && profile.marital_status} onChange={() => handleChange("marital_status")} className="ring-1 ring-black rounded px-2 py-2 bg-white dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary" required>
                             <option value="">--Marital Status--</option>
                             <option value="single">Single</option>
@@ -65,36 +68,31 @@ export default function ApplicationPg1({ profile, initialValues, setInitialValue
                             <option value="divorced">Divorced</option>
                         </select>
                     </div>
-                    <div className='flex flex-col w-56 '>
-                    <label htmlFor="" className=' text-sm'>Number of dependents</label>
-                    <input type="text" name="no_of_dependents" id="" placeholder='No. of dependants' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary' 
-                        onChange={handleChange("no_of_dependents")}
-                        required
-                    />
-                    </div>
+
+                    <InputField errors={errors} touched={touched} handleChange={handleChange}  handleBlur={handleBlur} reference="no_of_dependents"  label="Number of dependents" placeholder="Enter number"/>
+
                 </div>
         </div>
         <div className='mb-3'>
                 <h1 className='font-semibold'>Physical Address</h1>
                 <div className='flex flex-wrap gap-5 m-2'>
-                    <div className='flex flex-col w-56'>
-                        <label htmlFor="" className='text-sm'>Town</label>
-                        <input type="text" name="town" id="" placeholder='Enter your town' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary' onChange={handleChange("town")}/>
-                    </div>
+
+                <InputField errors={errors} touched={touched} handleChange={handleChange}  handleBlur={handleBlur} reference="town"  label="Town" placeholder="Enter town"/>
+
                     <div className='flex flex-col w-56 '>
-                    <label htmlFor="" className='text-sm'>Estate</label>
+                    <label className='text-sm'>Estate</label>
                     <input type="text" name="estate" id="" placeholder='Enter estate name' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary' onChange={handleChange("estate")} />
                     </div>
                     <div className='flex flex-col w-56 '>
-                    <label htmlFor="" className='text-sm'>Street</label>
+                    <label className='text-sm'>Street</label>
                     <input type="text" name="street" id="" placeholder='Enter street name' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary' onChange={handleChange("street")} />
                     </div>
                     <div className='flex flex-col w-56 '>
-                    <label htmlFor="" className='text-sm'>House Number</label>
+                    <label className='text-sm'>House Number</label>
                     <input type="text" name="house_no" id="" placeholder='Enter house no.' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary' onChange={handleChange("house_no")} />
                     </div>
                     <div className='flex flex-col w-56 '>
-                        <label htmlFor="" className='text-sm'>Ownership</label>
+                        <label className='text-sm'>Ownership</label>
                         <div className='flex justify-between'>
                             <div className='flex gap-1'>
                                 <input type="radio" id="owned" name="ownership" value="Owned" className='w-4 h-4' onChange={handleChange("ownership")}/>
@@ -107,7 +105,7 @@ export default function ApplicationPg1({ profile, initialValues, setInitialValue
                         </div>
                     </div>
                     <div className='flex flex-col w-56 '>
-                    <label htmlFor="" className='text-sm'>Years Spent</label>
+                    <label className='text-sm'>Years Spent</label>
                     <input type="text" name="years_spent" id="" placeholder='Years Spent' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary'onChange={handleChange("years_spent")} />
                     </div>
                 </div>
@@ -116,37 +114,33 @@ export default function ApplicationPg1({ profile, initialValues, setInitialValue
                 <h1 className='font-semibold'>Next of Kin</h1>
                 <div className='flex flex-wrap gap-5 m-2'>
                     <div className='flex flex-col w-56'>
-                    <label htmlFor="" className='text-sm'>Name</label>
-                    <input type="text" name="kin_name" id="" placeholder='Enter name' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary' onChange={handleChange("kin_name")} />
+                    <label className='text-sm'>Name</label>
+                    <input type="text" name="kin_name" id="" placeholder='Enter name' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary' onChange={handleChange("kin_name")} required />
                     </div>
                     <div className='flex flex-col w-56 '>
-                    <label htmlFor="" className=' text-sm'>Profession</label>
+                    <label className=' text-sm'>Profession</label>
                     <input type="text" name="kin_profession" id="" placeholder='Enter profession' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary' onChange={handleChange("kin_profession")}/>
                     </div>
-                    <div className='flex flex-col w-56 '>
-                    <label htmlFor="" className=' text-sm'>Contact</label>
-                    <input type="text" name="kin_contact" id="" placeholder='Enter contact' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary' onChange={handleChange("kin_contact")} />
-                    </div>
+
+                    <InputField errors={errors} touched={touched} handleChange={handleChange}  handleBlur={handleBlur} reference="kin_contact"  label="Contact" placeholder="Enter number"/>
+
                 </div>
             </div>
             <div className='mb-3'>
                 <h1 className='font-semibold'>Spouse</h1>
-                <div className='m-2'>
-                <div className='flex flex-wrap gap-5'>
+                <div className='m-2 flex flex-wrap gap-5'>
                     <div className='flex flex-col w-56'>
-                    <label htmlFor="" className='text-sm'>Name</label>
-                    <input type="text" name="spouse_name" id="" placeholder='Enter name' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary' onChange={handleChange("spouse-name")} />
+                    <label className='text-sm'>Name</label>
+                    <input type="text" name="spouse_name" id="" placeholder='Enter name' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary' onChange={handleChange("spouse-name")} required />
                     </div>
                     <div className='flex flex-col w-56 '>
-                    <label htmlFor="" className=' text-sm'>Profession</label>
+                    <label className=' text-sm'>Profession</label>
                     <input type="text" name="spouse_profession" id="" placeholder='Enter profession' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary' onChange={handleChange("spouse_profession")} />
                     </div>
-                    <div className='flex flex-col w-56 '>
-                    <label htmlFor="" className='text-sm'>Contact</label>
-                    <input type="text" name="spouse_contact" id="" placeholder='Enter contact' className='ring-1 ring-black rounded px-2 py-1 dark:bg-dark-bg-600 focus:outline-none focus:ring-2 focus:ring-primary' onChange={handleChange("spouse_contact")} />
-                    </div>
+
+                    <InputField errors={errors} touched={touched} handleChange={handleChange}  handleBlur={handleBlur} reference="spouse_contact"  label="Contact" placeholder="Enter number"/>
+
                 </div>
-            </div>
         </div>
         <div className='flex justify-end w-full'>
             <input
