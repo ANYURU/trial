4248d0e8542748293ff8f69a19729ea2229808f0
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2022-09-15 15:42:51 +0300

    Expansion, collapse of loans amortization and payments

--- src/components/Modals/LoanModal.jsx
+++ src/components/Modals/LoanModal.jsx
@@ -178,7 +178,7 @@ export default function LoanModal({ passed, setLoanModal, loan: {loan, payments}
               className="cursor-pointer"
               type="button"
               onClick={() => {
-                setAmortExpand(!repaymentExpand)
+                setRepaymentExpand(!repaymentExpand)
               }}
             >{repaymentExpand ? "collapse" : "expand"}</button>
           }
