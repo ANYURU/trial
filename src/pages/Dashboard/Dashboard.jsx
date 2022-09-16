d1fb3d4d3385a61c7fe3898776b11245eed1bd1e
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2022-09-16 14:48:32 +0300

    Sidebar

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -11,10 +11,10 @@ import SuperAdDashboard from "./SuperAdDashboard";
 import { Spinner } from "../../components";
 
 export default function Dashboard() {
-  const today = new Date()
+  const today = new Date();
 
   const matches = useMediaQuery("(min-width: 800px)");
-  const [user, profile ] = useOutletContext();
+  const [user, profile] = useOutletContext();
   const [myShares, setMyShares] = useState(0);
   const [saccosShares, setSaccosShares] = useState(0);
   const [monthlyShares, setMonthlyShares] = useState({
@@ -25,15 +25,15 @@ export default function Dashboard() {
     May: 0,
     Jun: 0,
     Jul: 0,
-    Aug: 0, 
+    Aug: 0,
     Sep: 0,
     Oct: 0,
     Nov: 0,
-    Dec: 0
-  })
-  const [ years, setYears ] = useState([])
-  const [ year, setYear ] = useState(today.getFullYear().toString())
-  const [ loading, setLoading ] = useState(true)
+    Dec: 0,
+  });
+  const [years, setYears] = useState([]);
+  const [year, setYear] = useState(today.getFullYear().toString());
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     document.title = "Dashboard - Bweyogere tuberebumu";
@@ -70,17 +70,20 @@ export default function Dashboard() {
   const get_monthly_shares = async () => {
     const { data, error } = await supabase.rpc("get_performance");
 
-    if ( error ) {
-      setLoading(false)
-      throw error
-    } 
+    if (error) {
+      setLoading(false);
+      throw error;
+    }
 
-    setLoading(false)
-    const { transactions,  years} = data
-    const sorted_years = (years && years.sort((a, b) => b-a)) ?? []
-    setYears(sorted_years)
-    
-    const filtered_transactions = await (transactions && transactions.filter(transaction => transaction?.year === year)) ?? []
+    setLoading(false);
+    const { transactions, years } = data;
+    const sorted_years = (years && years.sort((a, b) => b - a)) ?? [];
+    setYears(sorted_years);
+
+    const filtered_transactions =
+      (await (transactions &&
+        transactions.filter((transaction) => transaction?.year === year))) ??
+      [];
 
     let obj = {
       Jan: 0,
@@ -90,11 +93,11 @@ export default function Dashboard() {
       May: 0,
       Jun: 0,
       Jul: 0,
-      Aug: 0, 
+      Aug: 0,
       Sep: 0,
       Oct: 0,
       Nov: 0,
-      Dec: 0
+      Dec: 0,
     };
 
     if (filtered_transactions?.length > 0) {
@@ -177,7 +180,6 @@ export default function Dashboard() {
             matches && "overflow-y-hidden"
           }`}
         >
-
           {Object.keys(profile).length > 0 && !profile?.fullname && (
             <RegistrationModal />
           )}
@@ -196,20 +198,31 @@ export default function Dashboard() {
                 !matches && "flex-col w-full justify-center"
               }`}
             >
-              
               <div
                 className={`bg-white dark:bg-dark-bg-700 lg:w-6/12 md:w-6/12 sm:w-12/12  flex flex-col px-2 py-5 rounded-md justify-center items-center`}
               >
-              <div>
-                <label htmlFor="year" className="capitalize text-base font-medium">
-                  year  
-                  <select name="year" id="year" onChange={(event) => {
-                    setYear(event.target.value)
-                  }} className="ring-2 ring-primary mx-1 rounded">
-                  {years.map((year, index) => <option key={index} value={year}>{year}</option>)}
-                  </select>
-                </label>
-              </div>
+                <div>
+                  <label
+                    htmlFor="year"
+                    className="capitalize text-base font-medium"
+                  >
+                    year
+                    <select
+                      name="year"
+                      id="year"
+                      onChange={(event) => {
+                        setYear(event.target.value);
+                      }}
+                      className="ring-2 ring-primary mx-1 rounded"
+                    >
+                      {years.map((year, index) => (
+                        <option key={index} value={year}>
+                          {year}
+                        </option>
+                      ))}
+                    </select>
+                  </label>
+                </div>
                 <div className="w-full h-full p-4">
                   <Line data={data2} options={options2} />
                 </div>
@@ -228,16 +241,14 @@ export default function Dashboard() {
     } else {
       return <SuperAdDashboard />;
     }
-  }
-  else {
-    if(profile?.fullname) {
+  } else {
+    if (profile?.fullname) {
       return (
         <div
           className={`mx-5 mb-2 my-2 md:h-[calc(100vh-70px)] ${
             matches && "overflow-y-hidden"
           }`}
         >
-
           {Object.keys(profile).length > 0 && !profile?.fullname && (
             <RegistrationModal />
           )}
