b3f07742fc013044f460382ec72d2e5c85ed3397
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2022-10-28 13:01:48 +0300

    Guarantors and bank loans

--- src/pages/Loans/LoanVerify.jsx
+++ src/pages/Loans/LoanVerify.jsx
@@ -77,7 +77,7 @@ export default function LoanVerify() {
       .single();
 
       if( error ) throw error;
-
+      console.log(data)
       return data;
   };
 
@@ -86,10 +86,16 @@ export default function LoanVerify() {
     const { data, error } = await supabase.rpc('approve_loan',  {members_id:loan.application_meta.applicant_id, application:loan.application_id})
 
     if(error) throw error
+    console.log("Here: ",{...loan, ...data})
     toast.success(`Loan has been approved`,{
       position: 'top-center'
     })
-    console.log(data)
+
+    setLoan(loan => ({...loan, ...data}))
+  }
+
+  const rejectLoanApplication = async() => {
+    const {data, error} = await supabase
   }
 
   return (
@@ -109,14 +115,14 @@ export default function LoanVerify() {
                 's Loan Request
                 <span
                   className={` py-1 px-2 rounded-lg text-white text-xs ml-1 ${
-                    !loan.reviewed
+                    !loan?.reviewed
                       ? "bg-yellow-400"
                       : loan.application_meta.review_status === "approved"
                       ? "bg-green-400"
                       : "bg-red-400"
                   }`}
                 >
-                  {!loan.reviewed
+                  {!loan?.reviewed
                     ? "Pending"
                     : loan.application_meta.review_status === "approved"
                     ? "Approved"
@@ -126,14 +132,14 @@ export default function LoanVerify() {
 
               <div className="outline outline-1 outline-gray-100 p-3">
                 <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
-                  <p className="col-span-2">created_at:</p>
+                  <p className="col-span-2">Application ID:</p>
+                  <p className="font-bold col-span-3">{loan.app_id}</p>
+                </div>
+                <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
+                  <p className="col-span-2">Application Date:</p>
                   <p className="font-bold col-span-3">
                     {moment(loan.created_at).format("DD-MM-YYYY")}
                   </p>
-                </div>
-                <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
-                  <p className="col-span-2">Application ID:</p>
-                  <p className="font-bold col-span-3">{loan.app_id}</p>
                 </div>
                 <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
                   <p className="col-span-2">Applicants Name:</p>
@@ -143,8 +149,14 @@ export default function LoanVerify() {
                 </div>
                 <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
                   <p className="col-span-2">Position in SACCO:</p>
-                  <p className="font-bold col-span-3">
-                    {loan.application_meta.position_in_sacco}
+                  <p className="font-bold col-span-3 capitalize">
+                    {loan?.application_meta?.position_in_sacco}
+                  </p>
+                </div>
+                <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
+                  <p className="col-span-2">Loan Type:</p>
+                  <p className="font-bold col-span-3 capitalize">
+                    {loan.application_meta?.loan_type}
                   </p>
                 </div>
                 <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
@@ -161,6 +173,12 @@ export default function LoanVerify() {
                   </p>
                 </div>
                 <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
+                  <p className="col-span-2">Loan Purpose:</p>
+                  <p className="font-bold col-span-3">
+                    {loan.application_meta.loan_purpose}
+                  </p>
+                </div>
+                <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
                   <p className="col-span-2">Assets:</p>
                   <p className="font-bold col-span-3">
                     {loan.application_meta.asset1},{" "}
@@ -168,13 +186,7 @@ export default function LoanVerify() {
                     {loan.application_meta.asset3}
                   </p>
                 </div>
-                <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
-                  <p className="col-span-2">Loan Purpose:</p>
-                  <p className="font-bold col-span-3">
-                    {loan.application_meta.loan_purpose}
-                  </p>
-                </div>
-
+                
                 <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
                   <p className="col-span-2">Landline:</p>
                   <p className="font-bold col-span-3">
@@ -182,9 +194,33 @@ export default function LoanVerify() {
                   </p>
                 </div>
                 <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
-                  <p className="col-span-2">Town:</p>
-                  <p className="font-bold col-span-3">
-                    {loan.application_meta?.town}
+                  <p className="col-span-2">District:</p>
+                  <p className="font-bold col-span-3 capitalize">
+                    {loan.application_meta?.district}
+                  </p>
+                </div>
+                <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
+                  <p className="col-span-2">County:</p>
+                  <p className="font-bold col-span-3 capitalize">
+                    {loan.application_meta?.county}
+                  </p>
+                </div>
+                <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
+                  <p className="col-span-2">Sub County:</p>
+                  <p className="font-bold col-span-3 capitalize">
+                    {loan.application_meta?.sub_county}
+                  </p>
+                </div>
+                <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
+                  <p className="col-span-2">Parish:</p>
+                  <p className="font-bold col-span-3 capitalize">
+                    {loan.application_meta?.parish}
+                  </p>
+                </div>
+                <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
+                  <p className="col-span-2">SubParish:</p>
+                  <p className="font-bold col-span-3 capitalize">
+                    {loan.application_meta?.sub_parish}
                   </p>
                 </div>
 
@@ -200,31 +236,108 @@ export default function LoanVerify() {
                     {loan.application_meta?.kin_contact}
                   </p>
                 </div>
-                <div className="grid grid-cols-5 gap-2 mb-5 justify-start w-full">
-                  <p className="col-span-2">Next of Kin Profession:</p>
+                <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
+                  <p className="col-span-2">Interest Rate:</p>
                   <p className="font-bold col-span-3">
-                    {loan.application_meta?.kin_profession}
+                    {loan.application_meta?.interest_rate}%
                   </p>
                 </div>
+                <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
+                  <p className="col-span-2">Securities:</p>
+                  <p className="font-bold col-span-3">
+                  {loan.application_meta?.securities.map((security, index) => {
+                  return <span key={index} className="capitalize">{security}, </span>
+                })}
+                  </p>
+                </div>
+                
+                <div className="border-t-[0.5px] pt-2 mt-2">
+                  <h1>Guarantors: </h1>
+                  {
+                    loan.application_meta?.guarantors &&
+                    loan.application_meta.guarantors.map((guarantor, index)=> {
+                      return <>
+                        <div className="grid grid-cols-8 gap-2 mb-2 justify-start w-full">
+                          <p className="col-span-1 flex items-center justify-center font-bold"> {index + 1}</p>
+                          <div className="col-span-2 flex flex-col mb-2 justify-start w-full">
+                            <p className="col-span-2 font-bold">Name</p>
+                            <p className="font-normal col-span-3">
+                              {guarantor.name}
+                            </p>
+                          </div>
+                          <div className="  flex flex-col mb-2 justify-start w-full">
+                            <p className="col-span-2 font-bold">Contact</p>
+                            <p className="font-normal col-span-3">
+                              {guarantor.contact}
+                            </p>
+                          </div>
+                        </div>
+                      </>
+                    })
+                  }
+                </div>
+                {
+                  loan.application_meta?.bank_loans && loan.application_meta.bank_loans.length > 0 && 
+                  <div className="border-t-[0.5px] pt-2 mt-2">
+                    <h1>Other Bank Loans: </h1>
+                    {
+                      loan.application_meta.bank_loans.filter(loan => loan?.name?.length > 0).map((loan, index)=> {
+                        return <>
+                          <div className="grid grid-cols-8 gap-2 mb-2 justify-start w-full">
+                            <p className="col-span-1 flex items-center justify-center font-bold"> {index + 1}</p>
+                            <div className="col-span-2 flex flex-col mb-2 justify-start w-full">
+                              <p className="col-span-1 font-bold">Name</p>
+                              <p className="font-normal col-span-3">
+                                {loan.name}
+                              </p>
+                            </div>
+                            <div className="  flex flex-col mb-2 justify-start w-full">
+                              <p className="col-span-2 font-bold">balance</p>
+                              <p className="font-normal col-span-3">
+                                {loan.balance}
+                              </p>
+                            </div>
+                            <div className="  flex flex-col mb-2 justify-start w-full">
+                              <p className="col-span-2 font-bold">Date granted</p>
+                              <p className="font-normal col-span-3">
+                                {loan.date_granted}
+                              </p>
+                            </div>
+                            <div className="  flex flex-col mb-2 justify-start w-full">
+                              <p className="col-span-2 font-bold">balance</p>
+                              <p className="font-normal col-span-3">
+                                {loan.amount_advanced}
+                              </p>
+                            </div>
+                            <div className="  flex flex-col mb-2 justify-start w-full">
+                              <p className="col-span-2 font-bold">balance</p>
+                              <p className="font-normal col-span-3">
+                                {loan.repayment_period} months
+                              </p>
+                            </div>
+                          </div>              
+                        </>
+                      })
+                    }
+                  </div>
+                }
 
-                <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
-                  <p className="col-span-2">Spouse:</p>
-                  <p className="font-bold col-span-3">
-                    {loan.application_meta?.spouse_name}
-                  </p>
-                </div>
-                <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
-                  <p className="col-span-2">Next of spouse Contact:</p>
-                  <p className="font-bold col-span-3">
-                    {loan.application_meta?.spouse_contact}
-                  </p>
-                </div>
-                <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
-                  <p className="col-span-2">Next of spouse Profession:</p>
-                  <p className="font-bold col-span-3">
-                    {loan.application_meta?.spouse_profession}
-                  </p>
-                </div>
+                {
+                  loan.application_meta?.spouse_name && <>
+                    <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
+                      <p className="col-span-2">Spouse:</p>
+                      <p className="font-bold col-span-3">
+                        {loan.application_meta?.spouse_name}
+                      </p>
+                    </div>
+                    <div className="grid grid-cols-5 gap-2 mb-2 justify-start w-full">
+                      <p className="col-span-2">Next of spouse Contact:</p>
+                      <p className="font-bold col-span-3">
+                        {loan.application_meta?.spouse_contact}
+                      </p>
+                    </div>
+                  </>
+                }
                 {
                   bankStatementURL && (
                   <>
@@ -283,20 +396,23 @@ export default function LoanVerify() {
                 }
               </div>
             </div>
-            <div className="flex gap-10 justify-end items-center mt-3">
-              <button
-                className="bg-accent-red inline-flex items-center justify-center  text-white text-base font-medium px-4 py-2"
-                // onClick={rejectLoanPaymentTransaction}
-              >
-                Reject
-              </button>
-              <button
-                className="bg-green-600 inline-flex items-center justify-center  text-white text-base font-medium px-4 py-2"
-                onClick={approveLoanPaymentTransaction}
-              >
-                Approve
-              </button>
-            </div>
+            {
+              (loan?.application_meta?.review_status !== "rejected" && loan?.application_meta?.review_status !== "approved") && 
+              <div className="flex gap-10 justify-end items-center mt-3">
+                <button
+                  className="bg-accent-red inline-flex items-center justify-center  text-white text-base font-medium px-4 py-2"
+                  // onClick={rejectLoanPaymentTransaction}
+                >
+                  Reject
+                </button>
+                <button
+                  className="bg-green-600 inline-flex items-center justify-center  text-white text-base font-medium px-4 py-2"
+                  onClick={approveLoanPaymentTransaction}
+                >
+                  Approve
+                </button>
+              </div>
+            }
           </div>
         ) : (
           <Spinner />
